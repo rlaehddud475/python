564c5d96b3e785083877c7d6ecae90e70a695734
rlaehddud <6070kjh475@naver.com>
2026-09-02 15:38:00 +0900

    .

---
+++ two/Ex01.py
@@ -0,0 +1,20 @@
+fruits=["사과", "바나나", "포도"]
+fruits[1]="딸기"
+fruits.append("수박")
+print(fruits)
+
+
+student={
+    "name":"홍길동",
+    "age":30,  
+    "major":"AI"
+}
+print(student["name"])
+
+student["age"]=21
+student["grade"]=2
+
+print(student)
+
+numbvers=[1,2,2,3,3,4]
+print(numbvers)
