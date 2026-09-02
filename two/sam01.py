4d19a0cfe5fa7090614e7c3024e007fff4b03c79
rlaehddud <6070kjh475@naver.com>
2026-09-02 13:46:52 +0900

    ,

---
+++ two/sam01.py
@@ -0,0 +1,21 @@
+a = [1, 2, 3, ['a', 'b', 'c'], 4, 5]
+a[2:5]
+a[3][:2]
+a = [1, 2, 3]
+b = [4, 5, 6]
+a + b
+len(a)
+a[2] = 4
+print(a)
+str(a[2]) + "hi"
+del a[1]
+print(a)
+a = [1, 2, 3, 4, 5]
+del a[2:]
+a = [1, 2, 3]
+a.append(4)
+a.append([5, 6])
+print(a)
+a = [1, 4, 3, 2]
+a.sort()
+print(a)
