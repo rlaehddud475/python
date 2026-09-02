4921a6eb0dc8176cf4bf2c5a16cc33ffeb02d1a4
rlaehddud <6070kjh475@naver.com>
2026-09-02 14:16:41 +0900

    ,

--- two/sam01.py
+++ two/sam01.py
@@ -19,3 +19,23 @@ print(a)
 a = [1, 4, 3, 2]
 a.sort()
 print(a)
+a = ['a', 'c', 'b']
+a.reverse()
+print(a)
+a = [1, 2, 3]
+a.index(3)
+a.index(1)
+a.index(0)
+a.insert(0, 4)
+a.insert(3, 5)
+a = [1, 2, 3, 1, 2, 3]
+a.remove(3)
+a.remove(3)
+a.pop()
+a.pop(1)
+a = [1, 2, 3, 1]
+a.count(1)
+ a = [1, 2, 3]
+a.extend([4, 5])
+b = [6, 7]
+a.extend(b)
---
+++ two/sam03.py
@@ -0,0 +1,3 @@
+dic = {'name': 'pey', 'phone': '010-9999-1234', 'birth': '1118'}
+
+print(dic)
