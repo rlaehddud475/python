0e30aec3d168a562533f39b12c95cb156f41dde2
rlaehddud <6070kjh475@naver.com>
2026-09-16 13:21:59 +0900

    .

---
+++ four/sam1.py
@@ -0,0 +1,6 @@
+def add(a,b):
+    return a + b
+a = 3
+b = 4
+c = add(a, b)  # add(3, 4)의 반환값을 c에 대입
+print(c)
---
+++ three/Exam04.py
@@ -0,0 +1,7 @@
+result = 0
+i=1
+while i <= 100:
+    if i%3==0:
+        result += i
+    i+=1
+print(result)
